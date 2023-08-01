099d7301e1744f60d03eaf78ceffa235f31b0923
Jack Wilsdon <jack.wilsdon@gmail.com>
2023-08-02 00:20:28 +0100

    Add custom version marker

--- src/displayapp/screens/SystemInfo.cpp
+++ src/displayapp/screens/SystemInfo.cpp
@@ -80,7 +80,7 @@ std::unique_ptr<Screen> SystemInfo::CreateScreen1() {
   lv_label_set_recolor(label, true);
   lv_label_set_text_fmt(label,
                         "#FFFF00 InfiniTime#\n\n"
-                        "#808080 Version# %ld.%ld.%ld\n"
+                        "#808080 Version# %ld.%ld.%ld#FF0000 -jw\n"
                         "#808080 Short Ref# %s\n"
                         "#808080 Build date#\n"
                         "%s\n"
